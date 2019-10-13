adbf34c80980072f8a43b730ecfae7d9bff9995e
Timothy <timothycdykes@gmail.com>
2019-10-13 13:04:21 -0400

    Added algorithm: calculate nth Fibonacci number
    Added an algorithm for calculating the nth Fibonacci number using a recursive method.

---
+++ Miscellaneous/NthFibonacci.java
@@ -0,0 +1,21 @@
+class NthFibonacci {
+    
+    /*
+    * Print the nth Fibonacci number for a given number, n
+    * using recursion
+    */
+    
+    static int fib(int n) {
+            if (n <= 1) {
+                return n;
+            }
+            return fib(n - 1) + fib(n - 2);
+        }
+    
+    public static void main(String[ ] args) {
+        
+        int n = 5;
+        System.out.println(fib(n));
+        
+    }
+}
